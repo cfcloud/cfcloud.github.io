ae049344829e4870de61ac77e2dd5fc561b0ce79
Extraordinary Studio <56777627+jyc-jyc@users.noreply.github.com>
2021-12-23 23:54:36 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -4,3 +4,17 @@ function $(id){
 function $getClass(classname){
     return document.getElementsByClassName(classname)[0];
 }
+function $countday(ty, tm, td, nm){
+    var d = new Date();
+    var till = new Date(ty + "/" + tm + "/" + td);
+    if(d.getTime() > till.getTime()){
+        return "Now";
+    }
+    else{
+        var lefttime = till.getTime() - d.getTime();
+        leftd = Math.floor(lefttime/(1000*60*60*24));
+        if(nm != null)
+            return "距离" + nm + "还剩" + leftd + "天";
+        return "还剩" + leftd + "天";
+    }
+}
