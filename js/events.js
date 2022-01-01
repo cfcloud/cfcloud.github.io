ea1f944848dc4e16412ede5c6fead1abd30e7178
Extraordinary Studio <56777627+jyc-jyc@users.noreply.github.com>
2022-01-01 23:11:22 +0800

    Add files via upload

--- js/events.js
+++ js/events.js
@@ -2,26 +2,26 @@ var moreOn = false;
 function lmeDis(){
     if(moreOn == true){
         $("cov99").style.display = "none";
-        $getClass("left_menu_extra").style.display = "none";
+        $("left_menu_extra").style.display = "none";
         moreOn = false;
     }
 }
-$getClass("leftmenu").onmouseover = function(){
+$("leftmenu").onmouseover = function(){
     if(moreOn == false){
-    $getClass("leftmenu").style.background = "white";
-    $getClass("leftmenu").style.boxShadow = "0px 0px 10px #74ac5050";}
+    $("leftmenu").style.background = "white";
+    $("leftmenu").style.boxShadow = "0px 0px 10px #74ac5050";}
 }
-$getClass("leftmenu").onmouseout = function(){
+$("leftmenu").onmouseout = function(){
     if(moreOn == false){
-        $getClass("leftmenu").style.background = "rgba(255, 255, 255, 0.6)";
-        $getClass("leftmenu").style.boxShadow = "";
+        $("leftmenu").style.background = "rgba(255, 255, 255, 0.6)";
+        $("leftmenu").style.boxShadow = "";
     }
 }
 $("more").onclick = function(){
     if(moreOn == false){
         $("cov99").style.display = "inline";
-        $getClass("leftmenu").style.background = "white";
-        $getClass("left_menu_extra").style.display = "inline";
+        $("leftmenu").style.background = "white";
+        $("left_menu_extra").style.display = "inline";
         moreOn = true;
     }
     else{
@@ -31,12 +31,12 @@ $("more").onclick = function(){
 $("lmeClose").onclick = function(){
     if(moreOn == true){
         lmeDis();
-        $getClass("leftmenu").style.background = "rgba(255, 255, 255, 0.6)";
-        $getClass("leftmenu").style.boxShadow = "";
+        $("leftmenu").style.background = "rgba(255, 255, 255, 0.6)";
+        $("leftmenu").style.boxShadow = "";
     }
 }
 $("cov99").onclick = function(){
     lmeDis();
-    $getClass("leftmenu").style.background = "rgba(255, 255, 255, 0.6)";
-    $getClass("leftmenu").style.boxShadow = "";
+    $("leftmenu").style.background = "rgba(255, 255, 255, 0.6)";
+    $("leftmenu").style.boxShadow = "";
 }
--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,14 @@
-function $(id){
-    return document.getElementById(id);
-}
-function $getClass(classname){
-    return document.getElementsByClassName(classname)[0];
-}
-function $countday(ty, tm, td, nm){
-    var d = new Date();
-    var till = new Date(ty + "/" + tm + "/" + td);
-    if(d.getTime() > till.getTime()){
-        return "Now";
+function $(id, isClass){
+    var fromId = document.getElementById(id);
+    var fromClass = document.getElementsByClassName(id)[0];
+    if(isClass == true){
+        
+        return fromClass;
+    }
+    else if(fromId != null){
+        return fromId;
     }
     else{
-        var lefttime = till.getTime() - d.getTime();
-        leftd = Math.floor(lefttime/(1000*60*60*24));
-        if(nm != null)
-            return "距离" + nm + "还剩" + leftd + "天";
-        return "还剩" + leftd + "天";
+        return fromClass;
     }
 }
