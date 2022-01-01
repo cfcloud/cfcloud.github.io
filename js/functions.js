af83d3549ce5531478f4924b429b9f6a23fb21d4
Extraordinary Studio <56777627+jyc-jyc@users.noreply.github.com>
2022-01-02 01:11:55 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,11 @@
 function $(id, isClass){
-    var fromId = document.getElementById(id);
-    var fromClass = document.getElementsByClassName(id)[0];
     if(isClass == true){
-        
-        return fromClass;
+        return document.getElementsByClassName(id)[0];
     }
-    else if(fromId != null){
-        return fromId;
+    else if(document.getElementById(id) != null){
+        return document.getElementById(id);
     }
     else{
-        return fromClass;
+        return document.getElementsByClassName(id)[0];
     }
 }
