a2e7e15eecac189f387fd35d884db59b4a3393d2
Extraordinary Studio <56777627+jyc-jyc@users.noreply.github.com>
2022-01-02 01:59:59 +0800

    Create functions.js

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,20 @@ function $(id, isClass){
         return document.getElementsByClassName(id)[0];
     }
 }
+function $countday(ty, tm, td, nm, isNum){
+    var d = new Date();
+    var till = new Date(ty + "/" + tm + "/" + td);
+    var lefttime = till.getTime() - d.getTime();
+    leftd = Math.floor(lefttime/(1000*60*60*24));
+    if(isNum == true){
+        return leftd;
+    }
+    if(leftd <= 0){
+        return "Now";
+    }
+    else{
+        if(nm != null)
+            return "距离" + nm + "还剩" + leftd + "天";
+        return "还剩" + leftd + "天";
+    }
+}
